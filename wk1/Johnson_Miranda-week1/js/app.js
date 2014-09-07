275ea81b3fc0d77b7a2a468838022d34527de892
Mirajo <mmj_chicago@yahoo.com>
2014-09-07 05:15:13 -0400

    final submit
    final submit

--- wk1/Johnson_Miranda-week1/js/app.js
+++ wk1/Johnson_Miranda-week1/js/app.js
@@ -7,4 +7,19 @@
 function DBController($scope){
 
      $scope.listName;
+
+     $scope.listArray = ['tomatos', 'rice', 'apples'];
+
+     $scope.addName = function(){
+         $scope.listArray.push($scope.listName);
+
+         $scope.listName = '';
+
+     }
+
+    $scope.deleteName = function(deletedName){
+        var index = $scope.listArray.indexOf(deletedName);
+        $scope.listArray.splice(index,1);
+
+    }
  }
